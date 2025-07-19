bbf5476164a63fec21404c3c689b3c295166dad5
Ali-Dabiri <dabiri1380@gmail.com>
2025-07-19 05:28:31 +0330

    first commit for Roshan_Task02_01_00

---
+++ Roshan_Internship/Roshan_Task02_01_00/Task02_01_00_News_Builder_API/Task02_01_00_News_Page_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Task020100NewsPageAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Task02_01_00_News_Page_App'
